fcaffd52fc9de83577dcc458c9d8dff820f6a332
georgecalle <gcalle@gmail.com>
2023-12-29 18:35:51 -0500

    fixed counter and added to footer

--- website/index.js
+++ website/index.js
@@ -1,9 +1,9 @@
-//  javaScript Code for counter
+//  javaScript Code for view counter
 
-const counter = document.querySelector(".count-number");
+const counter = document.querySelector(".counter-number");
 async function updateCounter() {
     let response = await fetch("https://5u4p7upptq5fcjdwg5z3d672vy0yrjtm.lambda-url.us-east-1.on.aws/");
     let data = await response.json();
-    counter.innerHTML = '👀 Views: ${data}';
+    counter.innerHTML = `👀 Views: ${data}`;
 }
 updateCounter();
