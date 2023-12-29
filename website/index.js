d6d62b42bb08242f9e3a3e9723bfcffba76c2625
georgecalle <gcalle@gmail.com>
2023-12-29 17:04:03 -0500

    fixed typo on js

--- website/index.js
+++ website/index.js
@@ -2,10 +2,8 @@
 
 const counter = document.querySelector(".count-number");
 async function updateCounter() {
-    let resonse = await fetch(
-        "https://5u4p7upptq5fcjdwg5z3d672vy0yrjtm.lambda-url.us-east-1.on.aws/"
-    );
-    let data = await Response.json();
+    let response = await fetch("https://5u4p7upptq5fcjdwg5z3d672vy0yrjtm.lambda-url.us-east-1.on.aws/");
+    let data = await response.json();
     counter.innerHTML = '👀 Views: ${data}';
 }
 updateCounter();
