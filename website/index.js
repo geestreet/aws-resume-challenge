00ce55aec355b56ae2e5b6cd1dcae685fe528654
georgecalle <gcalle@gmail.com>
2023-12-29 16:51:00 -0500

    add index.js for counter

---
+++ website/index.js
@@ -0,0 +1,11 @@
+//  javaScript Code for counter
+
+const counter = document.querySelector(".count-number");
+async function updateCounter() {
+    let resonse = await fetch(
+        "https://5u4p7upptq5fcjdwg5z3d672vy0yrjtm.lambda-url.us-east-1.on.aws/"
+    );
+    let data = await Response.json();
+    counter.innerHTML = '👀 Views: ${data}`;  
+}
+updateCounter();
