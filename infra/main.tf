553e9e991bc15a22ef084ba34fec6f4b12c72a64
georgecalle <gcalle@gmail.com>
2023-12-30 18:43:16 -0500

    found typo in version date

--- infra/main.tf
+++ infra/main.tf
@@ -12,7 +12,7 @@ resource "aws_iam_role" "iam_for_lambda" {
 
   assume_role_policy = <<EOF
 {
-  "Version" : "2012-10-117",
+  "Version" : "2012-10-17",
   "Statement": [
     {
       "Action": "sts:AssumeRole",
